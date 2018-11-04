06fed517f58a9ba6e09b076b1c2acd7873f2ed84
Ryan Mui <reachryanmui@gmail.com>
2018-11-04 00:00:42 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -25,7 +25,7 @@ var tomatoSauceIngredients = [
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 
 function addElementToBeginningOfArray(array, element){
-    return ["element", ...array]
+    return array2 = ["element", ...array]
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
