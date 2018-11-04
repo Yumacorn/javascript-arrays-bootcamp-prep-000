adc7fe7cb91e60f1f47cda1bb18663029b17e23e
Ryan Mui <reachryanmui@gmail.com>
2018-11-04 00:59:58 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -53,7 +53,7 @@ function destructivelyRemoveElementFromBeginningOfArray(array) {
 
 var cats = ["Milo", "Garfield", "Otis", "Rog", "Fi"]
 
-cats = cats.slice(3,0) // ["Garfield", "Otis"]
+cats = cats.slice(3,1) // ["Garfield", "Otis"]
 console.log(cats)
 cats // ["Milo", "Garfield", "Otis"]
 //function destructivelyRemoveElementFromBeginningOfArray(array) {
