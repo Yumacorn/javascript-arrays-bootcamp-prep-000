f258d33c328eca1c1cd00f8b62e31352b2d1dcb8
Ryan Mui <reachryanmui@gmail.com>
2018-11-04 00:40:07 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -47,7 +47,7 @@ function accessElementInArray(array, index) {
 }
 
 function destructivelyRemoveElementFromBeginningOfArray(array) {
-
+    return array.shift()
 }
 
 function destructivelyRemoveElementFromBeginningOfArray(array) {
